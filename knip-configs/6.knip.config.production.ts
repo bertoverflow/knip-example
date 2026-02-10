5f91ab856d1e4a0e3951dd9110013353a2c3cf07
Bertram Vogel <bertram.vogel@codecentric.de>
2026-02-10 17:12:44 +0100

    prepare short-version of project for demo

--- knip-configs/6.knip.config.production.ts
+++
@@ -1,9 +0,0 @@
-import type { KnipConfig } from "knip";
-import defaultConfig from "./knip.config.js";
-
-const config: KnipConfig = {
-  ...defaultConfig,
-  ignoreExportsUsedInFile: true,
-};
-
-export default config;
